c8a97c1721f2deaac898c70db13114fff73c5116
alexlo95 <jlozada1605@hotmail.com>
2014-11-07 04:39:33 -0500

    Cambios necesarios para el metodo que se encarga de desactivar el recurso correspondiente a "my SQL"

--- RigitalApp.mock/src/main/java/co/edu/uniandes/csw/grupo/mysql/logic/mock/MySQLMockLogicService.java
+++ RigitalApp.mock/src/main/java/co/edu/uniandes/csw/grupo/mysql/logic/mock/MySQLMockLogicService.java
@@ -40,5 +40,9 @@ public class MySQLMockLogicService extends _MySQLMockLogicService implements IMy
     public MySQLPageDTO getMySQLsByParameters(String descripcionDestino, String servidor, String name, String descripcion, String proposito, String caracteristicas, String pgwebId, String paginawebId, String encargadoId, String fechaCreacion1, String fechaCreacion2, String estaDestruido) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    public void desactivarRecurso(Long mysql) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 	
 }
